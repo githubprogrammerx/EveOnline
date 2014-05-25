5d08ccfae050c60a5c548887b459a98723935e3b
githubprogammerx <petertimusk@programdocumentation.com>
2014-05-25 19:47:07 -0400

    reads in files
    Starting the read in of 1000's of files

---
+++ ReadinLogfile.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/ptimusk/Documents/GitHub/EveOnline")
+##run_analysis
+testlog1<- read.delim("C:/Users/ptimusk/Dropbox/EveOnline/logs/IBMThinkCenter/logs/Gamelogs/20080204_063803.txt", sep="" , skip=4, header=FALSE)
+testlog2<- read.delim("C:/Users/ptimusk/Dropbox/EveOnline/logs/IBMThinkCenter/logs/Gamelogs/20080204_064426.txt", sep="" , skip=4, header=FALSE)
+testlog3<- read.delim("C:/Users/ptimusk/Dropbox/EveOnline/logs/IBMThinkCenter/logs/Gamelogs/20080204_065856.txt", sep="" , skip=4, header=FALSE)
